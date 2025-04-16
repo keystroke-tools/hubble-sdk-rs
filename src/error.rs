b8d5e9e23b22e5903e4e2ab132d765c35d583cdc
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-04-16 19:04:13 +0100

    Use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,31 @@
-#[derive(Debug)]
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum Error {
+    #[error("Failed to read/parse Cap'n Proto message: {0}")]
     Capnp(capnp::Error),
+
+    #[error("Failed to read UTF-8 string: {0}")]
     Utf8(std::str::Utf8Error),
+
+    #[error("An I/O error occured: {0}")]
     Io(std::io::Error),
+
+    #[error("A runtime error occured: {0}")]
     PluginError(String),
+
+    #[error("Failed to read a string from memory: {reason}")]
+    ReadStringError { reason: String },
+
+    #[error("No valid UTF-8 string found in memory")]
+    EmptyStringError,
+
+    #[error("A memory-related error occured: {0}")]
     MemoryError(String),
+
+    #[error("Failed to read memory")]
     ReadMemoryError(String),
+
+    #[error("Failed to allocate memory")]
     MemoryAllocationFailed,
 }
